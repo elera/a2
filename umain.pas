1cfa83f48f88ada1e0ceda29a02028268ed325ad
elera <hs.fatih.kilic@gmail.com>
2017-12-12 11:21:04 +0300

    Revert "0.0.12.2017"
    This reverts commit 159115fac0fbd5c85d6702f21b041ee74f6206e9.

--- umain.pas
+++ umain.pas
@@ -9,11 +9,7 @@ uses
   ComCtrls, ExtCtrls;
 
 type
-
-  { TfrmMain }
-
   TfrmMain = class(TForm)
-    Button1: TButton;
     gbAsmLines: TGroupBox;
     gbAsmLinesOutput: TGroupBox;
     mmAsmLines: TMemo;
@@ -21,7 +17,6 @@ type
     spMain: TSplitter;
     sbMain: TStatusBar;
     tbMain: TToolBar;
-    procedure Button1Click(Sender: TObject);
   private
 
   public
@@ -34,42 +29,5 @@ var
 implementation
 
 {$R *.lfm}
-
-uses profunc;
-
-procedure TfrmMain.Button1Click(Sender: TObject);
-var
-  CurrentLineCount, TotalLineCount: Integer;
-  CodeData, Data: string;
-begin
-
-  mmAsmLinesOutput.Clear;
-
-  TotalLineCount := mmAsmLines.Lines.Count;
-  CurrentLineCount := 0;
-
-  while CurrentLineCount < TotalLineCount do
-  begin
-
-    Data := mmAsmLines.Lines[CurrentLineCount];
-    CodeData := Trim(Data);
-    if(Length(CodeData) > 0) then
-    begin
-
-      if(CodeData[1] = ';') then
-
-        mmAsmLinesOutput.Lines.Add('Açıklama: ' + Data)
-
-      else
-      begin
-
-        Data := GetOpcode(CodeData);
-        mmAsmLinesOutput.Lines.Add('Kod: ' + Data);
-      end;
-    end;
-
-    Inc(CurrentLineCount);
-  end;
-end;
 
 end.
